2c184c568bf46533f1641922827d11b9e0ecaf4d
Golang Uzbekistan <golanguzb70@gmail.com>
2023-08-02 10:22:04 +0500

    update order of type

--- cache.go
+++ cache.go
@@ -8,41 +8,41 @@ type Node[T any, K comparable] struct {
 	key     K
 	value   T
 	created time.Time
-	timeout  int // seconds
+	timeout int // seconds
 	prev,
 	next *Node[T, K]
 }
 
-type LRUCache[T any, K comparable] struct {
+type LRUCache[K comparable, T any] struct {
 	space      int
 	cache      map[K]*Node[T, K]
 	head, tail *Node[T, K]
 }
 
-func New[K comparable, T any](capacity, timeout int) LRUCache[T, K] {
+func New[K comparable, T any](capacity, timeout int) LRUCache[K, T] {
 	cache := map[K]*Node[T, K]{}
 	var zero T
 	var zeroKey K
 	head, tail := &Node[T, K]{zeroKey, zero, time.Now(), timeout, nil, nil}, &Node[T, K]{zeroKey, zero, time.Now(), timeout, nil, nil}
 	head.next = tail
 	tail.prev = head
-	return LRUCache[T, K]{capacity, cache, head, tail}
+	return LRUCache[K, T]{capacity, cache, head, tail}
 }
 
-func (lru *LRUCache[T, K]) AddNode(node *Node[T, K]) {
+func (lru *LRUCache[K, T]) AddNode(node *Node[T, K]) {
 	node.next = lru.head.next
 	node.prev = lru.head
 	node.next.prev = node
 	lru.head.next = node
 }
 
-func (lru *LRUCache[T, K]) RemoveNode(node *Node[T, K]) {
+func (lru *LRUCache[K, T]) RemoveNode(node *Node[T, K]) {
 	prev, next := node.prev, node.next
 	node.prev.next = next
 	node.next.prev = prev
 }
 
-func (lru *LRUCache[T, K]) Get(key K) (T, bool) {
+func (lru *LRUCache[K, T]) Get(key K) (T, bool) {
 	if node, ok := lru.cache[key]; ok {
 		lru.RemoveNode(node)
 		lru.AddNode(node)
@@ -53,7 +53,7 @@ func (lru *LRUCache[T, K]) Get(key K) (T, bool) {
 	}
 }
 
-func (lru *LRUCache[T, K]) Put(key K, value T) {
+func (lru *LRUCache[K, T]) Put(key K, value T) {
 	if node, ok := lru.cache[key]; ok {
 		node.value = value
 		lru.RemoveNode(node)
