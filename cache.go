ffc945183165bf990491d68dadfca6e210a646cb
Golang Uzbekistan <golanguzb70@gmail.com>
2023-08-02 10:29:20 +0500

    add Clear method

--- cache.go
+++ cache.go
@@ -8,7 +8,6 @@ type Node[T any, K comparable] struct {
 	key     K
 	value   T
 	created time.Time
-	timeout int // seconds
 	prev,
 	next *Node[T, K]
 }
@@ -17,16 +16,19 @@ type LRUCache[K comparable, T any] struct {
 	space      int
 	cache      map[K]*Node[T, K]
 	head, tail *Node[T, K]
+	timeout    int
 }
 
 func New[K comparable, T any](capacity, timeout int) LRUCache[K, T] {
 	cache := map[K]*Node[T, K]{}
-	var zero T
-	var zeroKey K
-	head, tail := &Node[T, K]{zeroKey, zero, time.Now(), timeout, nil, nil}, &Node[T, K]{zeroKey, zero, time.Now(), timeout, nil, nil}
+	var (
+		zero    T
+		zeroKey K
+	)
+	head, tail := &Node[T, K]{zeroKey, zero, time.Now(), nil, nil}, &Node[T, K]{zeroKey, zero, time.Now(), nil, nil}
 	head.next = tail
 	tail.prev = head
-	return LRUCache[K, T]{capacity, cache, head, tail}
+	return LRUCache[K, T]{capacity, cache, head, tail, timeout}
 }
 
 func (lru *LRUCache[K, T]) AddNode(node *Node[T, K]) {
@@ -66,8 +68,21 @@ func (lru *LRUCache[K, T]) Put(key K, value T) {
 		} else {
 			lru.space--
 		}
-		newNode := &Node[T, K]{key, value, time.Now(), 0, nil, nil}
+		newNode := &Node[T, K]{key, value, time.Now(), nil, nil}
 		lru.AddNode(newNode)
 		lru.cache[key] = newNode
 	}
+}
+
+func (lru *LRUCache[K, T]) Clear() {
+	for k := range lru.cache {
+		delete(lru.cache, k)
+	}
+	var (
+		zero    T
+		zeroKey K
+	)
+	head, tail := &Node[T, K]{zeroKey, zero, time.Now(), nil, nil}, &Node[T, K]{zeroKey, zero, time.Now(), nil, nil}
+	head.next = tail
+	tail.prev = head
 }
